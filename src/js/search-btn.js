96f7c0a5636b37a7e21404888e13ec3e0387b0d9
Gosteeva <olia.gosteeva@gmail.com>
2016-05-05 00:04:14 +0300

    improved toggle
    improved changing search and button

--- src/js/search-btn.js
+++ src/js/search-btn.js
@@ -1,13 +1,8 @@
 $(document).ready(function() {
-    $('.search').click(function() {
-      var val = $(this).text();
-      if (val == 'search'){
-        $('.btn').html("Search");
-        $(this).html("light a star");
-      }
-      else {
-        $('.btn').html("Light a star");
-        $(this).html("search");
-      }
+    $('.tg').click(function() {
+      $('.btn').toggle();
+      $('.search').toggle();
+      $('.s-txt').toggle();
+      $('.l-txt').toggle();
      })
 });
